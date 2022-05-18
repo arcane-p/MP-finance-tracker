13cd1144422546753792bfe0d7a15204a166f200
TheArcanePowers <leoman.coppi@gmail.com>
2022-05-18 19:24:01 +0100

    PDF reading and MP initialization

---
+++ assets/classes.py
@@ -0,0 +1,49 @@
+import re
+
+# chapters = {"Nil",
+#             "Employment and Earnings",
+#             ["(a) Support linked to an MP but received by a local party organisation or indirectly via a central party organisation",
+#              "(b) Any other support not included in Category 2(a)"],
+#             "Gifts, benefits and hospitality from UK sources",
+#             "Visits outside the UK",
+#             "Gifts and benefits from sources outside the UK",
+#             "Land and property portfolio: (i) value over £100,000 and/or (ii) giving rental income of over £10,000 a year",
+#             ["(i) Shareholdings: over 15% of issued share capital",
+#              "(ii) Other shareholdings, valued at more than £70,000"],
+#             "Miscellaneous",
+#             "Family members employed and paid from parliamentary expenses",
+#             "Family members engaged in lobbying the public sector on behalf of a third party or client"
+# }
+
+class MP:
+    def __init__(self, firstname, lastname):
+        self.firstname = firstname
+        self.lastname = lastname
+        print("MP Initialized: ", firstname, lastname)
+
+    def load_raw(self, raw_text: str):
+        print(f"Raw text for {self.firstname} recieved.")
+        return
+        # Check for NIL
+        # 1. Employment and Earnings
+        # 2. (a) Support linked to an MP but received by a local party organisation or indirectly via a central party organisation (b) Any other support not included in Category 2(a)
+        # 3. Gifts, benefits and hospitality from UK sources
+        # 4. Visits outside the UK
+        # 5. Gifts and benefits from sources outside the UK
+        # 6. Land and property portfolio: (i) value over £100,000 and/or (ii) giving rental income of over £10,000 a year
+        # 7. (i) Shareholdings: over 15% of issued share capital (ii) Other shareholdings, valued at more than £70,000
+        # 8. Miscellaneous
+        # 9. Family members employed and paid from parliamentary expenses
+        # 10. Family members engaged in lobbying the public sector on behalf of a third party or client
+        if "Nil" in raw_text:
+            final_results = None
+        pattern = re.compile(r"([1-9]|10)\. (E|G|V|L|M|F|\(i\)|\(a\)|\(ii\)|\(b\))((.|\n)*?)(?=([1-9]|10)\. (E|G|V|L|M|F|\(i\)|\(a\)|\(ii\)|\(b\)))", re.M)
+        result = pattern.findall(raw_text)
+        final_results = []
+        for i in result:
+            final_results.append("".join(i))
+        print(final_results)
+
+
+class Company:
+    pass
---
+++ main.py
@@ -0,0 +1,47 @@
+import pdfplumber
+from assets.classes import MP
+import re
+
+def load_pdf(file):
+    #TODO regex catches "Asylum and Migration Policy Project Refugee", "care of Embassy of Ukraine Ukraine", "owned by Manor Mews Medway", \
+    # "Christine Jardine Communications Trader", "Director of Eviivo Ltd notice", "from Analogue Electrics Ltd month", "Asylum and Migration Policy Refugee" \
+    # "as a secretary Coffey", "via JJC Holdings Ltd AF". Possibly limit it to 3 words?
+    mp_pattern = re.compile(r"([a-zA-Z]+?, [a-z A-Z]*?)(?=( \([a-z A-Z]*\)))", re.M)
+    with pdfplumber.open(f"{file}.pdf") as pdf:
+        queue_text = ""
+        for i in pdf.pages:
+            # Extract text & detect
+            page_text = i.extract_text()
+            detections = list(mp_pattern.finditer(page_text))
+
+            # QUEUE SECTION
+            if len(detections) == 0:  # On a full page
+                queue_text += page_text  # Add top to bottom of page to queue
+                continue  # go to next page w/ added queue
+            elif i != pdf.pages[0]:
+                queue_text += page_text[0:detections[0].start()]  # add page top to previous_mp's queue
+                mp = MP(firstname=name[1].strip(), lastname=name[0].strip())  # noqa: F821, name will always be set by previous iteration when page != 1
+                mp.load_raw(queue_text)  # load previous_mp
+
+            # all but last MP
+            for i in range(0, len(detections)-1):
+                name = detections[i].group().split(",")
+                mp = MP(firstname=name[1].strip(), lastname=name[0].strip())
+                mp.load_raw(page_text[detections[i].end():detections[i+1].start()])
+
+            if i == pdf.pages[-1]:  # last page
+                break  # Finished! no need to continue or create more queues
+
+            # from last mp to page end
+            name = detections[len(detections)-1].group().split(",")
+            queue_text = page_text[detections[len(detections)-1].end():len(page_text)]
+
+
+def main():
+    #url = input("URL: ")
+    text = load_pdf("220503")
+    print(text)
+
+
+if __name__ == "__main__":
+    main()
